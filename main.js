69302a20935aa85b3e7cdf3ca8fa898ecf629375
Mehnazar <161054226+Mehnazar@users.noreply.github.com>
2024-06-06 21:19:43 +0500

    todos list project

---
+++ main.js
@@ -0,0 +1,22 @@
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+    let addTask = await inquirer.prompt([
+        {
+            name: 'todo',
+            type: "input",
+            message: "what would you like to add in your todos?"
+        },
+        {
+            name: 'addMore',
+            type: "confirm",
+            message: "what would you like to add in your todos?",
+            default: "true"
+        }
+    ]);
+    todos.push(addTask.todo);
+    condition = addTask.addMore;
+    console.log(todos);
+}
+console.log(todos.length);
